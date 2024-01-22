c011df0292c9d6996e035090060c12598311090f
Adrian Alviento <adrianalviento@activate.sg>
2024-01-22 17:37:03 +0800

    Finish lesson 110

--- food/src/components/ResultsList.tsx
+++ food/src/components/ResultsList.tsx
@@ -4,9 +4,8 @@ import { result } from '../api/yelp';
 import ResultDetail from './ResultDetail';
 
 const ResultsList = ({ title, results } : { title: string, results: result[] }) => {
-    return <View>
+    return <View style={styles.container}>
         <Text style={styles.title}>{title}</Text>
-        <Text>Results: {results.length}</Text>
         <FlatList
             horizontal = {true}
             data = {results}
@@ -21,7 +20,12 @@ const ResultsList = ({ title, results } : { title: string, results: result[] }) 
 const styles = StyleSheet.create({
     title: {
         fontSize: 18,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        marginLeft: 15,
+        marginBottom: 5
+    },
+    container: {
+        marginBottom: 10
     }
 });
 
