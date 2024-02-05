ba239b124592dab44900b95cd8f4fe72d0ae5739
Adrian Alviento <adrianalviento@activate.sg>
2024-02-05 17:03:25 +0800

    Finish lesson 209

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -7,8 +7,30 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = (dispatch) => {
+  return ({ email, password }) => {
+    // make api req to sign up with that email and pw
+    // if we sign up, modify our state and say we are authenticated
+    // if signing up fails, reflect error somewhere
+  };
+};
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {
+    // try to sign in
+    // handle success by updating state
+    // handle failure by showing error message
+  };
+};
+
+const signout = (dispatch) => {
+  return () => {
+    // somehow sign out
+  };
+};
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
+  { signin, signout, signup },
   { isSignedIn: false }
 );
