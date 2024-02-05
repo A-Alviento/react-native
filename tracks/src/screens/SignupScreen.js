a7c437f8526baaa3f8835a4d8e8eed4c1e764046
Adrian Alviento <adrianalviento@activate.sg>
2024-02-05 13:08:36 +0800

    Finish lesson 207

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,41 +1,54 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Text, Input, Button } from 'react-native-elements'; 
-import Spacer from '../components/Spacer'
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
-    return (
-        <View style={styles.container}>
-            <Spacer>
-                <Text h3>Sign Up for Tracker</Text>
-            </Spacer>
-            <Input label="Email" />
-            <Spacer />
-            <Input label="Password" />
-            <Spacer>
-                <Button
-                    title="Sign Up"
-                    onPress={() => navigation.navigate('Signin')}
-                />
-            </Spacer>
-        </View>
-    );
-};  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+      <Input
+        label="Email"
+        value={email}
+        onChangeText={(newEmail) => setEmail(newEmail)}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <Spacer />
+      <Input
+        label="Password"
+        value={password}
+        onChangeText={(password) => setPassword(password)}
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry={true}
+      />
+      <Spacer>
+        <Button title="Sign Up" onPress={() => navigation.navigate("Signin")} />
+      </Spacer>
+    </View>
+  );
+};
 
 SignupScreen.navigationOptions = () => {
-    return {
-        headerShown: false,
-    };
+  return {
+    headerShown: false,
+  };
 };
 
 const styles = StyleSheet.create({
-    container: {
-        borderColor: 'red',
-        borderWidth: 10,
-        flex: 1,
-        justifyContent: 'center',
-        marginBottom: 200
-    }
+  container: {
+    borderColor: "red",
+    borderWidth: 10,
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 200,
+  },
 });
 
 export default SignupScreen;
