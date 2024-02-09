4484367c9b0d7426262f5dc4f22bfdcdef89d483
Adrian Alviento <adrianalviento@activate.sg>
2024-02-09 19:12:49 +0800

    Finish lesson 241

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -1,16 +1,37 @@
-import React from "react";
+import '../_mockLocation';
+import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-navigation";
+import * as Location from "expo-location";
 import Map from "../components/Map";
 
 const TrackCreateScreen = () => {
-    return (
-        <SafeAreaView forceInset={{ top: "always" }}>
-            <Text h1>Create a Track</Text>
-            <Map />
-        </SafeAreaView>
-    )
+  const [status, requestPermission] = Location.useBackgroundPermissions();
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestPermission();
+      if (!granted) {
+        throw new Error("Location permission not granted");
+      }
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
+  return (
+    <SafeAreaView forceInset={{ top: "always" }}>
+      <Text h1>Create a Track</Text>
+      <Map />
+      {err ? <Text>Please enable location services</Text> : null}
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({});
