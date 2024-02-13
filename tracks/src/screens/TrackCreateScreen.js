61e2b33f9ed35766dd2137dea024fc005c27db55
Adrian Alviento <adrianalviento@activate.sg>
2024-02-13 11:19:01 +0800

    Finish lesson 246

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -1,12 +1,14 @@
-import '../_mockLocation';
-import React, { useEffect, useState } from "react";
+import "../_mockLocation";
+import React, { useEffect, useState, useContext } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-navigation";
 import * as Location from "expo-location";
 import Map from "../components/Map";
+import { Context as LocationContext } from "../context/LocationContext";
 
 const TrackCreateScreen = () => {
+  const { addLocation } = useContext(LocationContext);
   const [status, requestPermission] = Location.useBackgroundPermissions();
   const [err, setErr] = useState(null);
 
@@ -16,6 +18,16 @@ const TrackCreateScreen = () => {
       if (!granted) {
         throw new Error("Location permission not granted");
       }
+      await Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10,
+        },
+        location => {
+          addLocation(location);
+        }
+      );
     } catch (e) {
       setErr(e);
     }
