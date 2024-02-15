3d97e512e2cf1b88d95e970586db0fc0cc1dbe8a
Adrian Alviento <adrianalviento@activate.sg>
2024-02-15 20:01:37 +0800

    Finish lesson 268

--- tracks/src/hooks/useSaveTrack.js
+++ tracks/src/hooks/useSaveTrack.js
@@ -1,13 +1,23 @@
 import { useContext } from "react";
 import { Context as TrackContext } from "../context/TrackContext";
 import { Context as LocationContext } from "../context/LocationContext";
+import { navigate } from "../navigationRef";
 
 export default () => {
   const { createTrack } = useContext(TrackContext);
-  const { state: { locations, name }} = useContext(LocationContext);
+  const {
+    state: { locations, name },
+    reset,
+  } = useContext(LocationContext);
 
-  const saveTrack = () => {
-    createTrack(name, locations);
+  const saveTrack = async () => {
+    // createTrack involves an async operation,
+    // so we need to use the await keyword if
+    // we want to wait for the operation to complete
+    // before moving on to the next line of code.
+    await createTrack(name, locations);
+    reset();
+    navigate("TrackList");
   };
 
   return [saveTrack];
