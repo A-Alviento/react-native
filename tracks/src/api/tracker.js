859e0d8f4a0a14b052d081e0afdfecaf38f4ddf7
Adrian Alviento <adrianalviento@activate.sg>
2024-02-15 19:50:37 +0800

    Finish lesson 267

--- tracks/src/api/tracker.js
+++ tracks/src/api/tracker.js
@@ -1,5 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default axios.create({
-    baseURL: 'https://1176-66-96-203-254.ngrok-free.app/'
-})
+const instance = axios.create({
+  baseURL: "https://1176-66-96-203-254.ngrok-free.app/",
+});
+
+instance.interceptors.request.use(
+  // first function called automatically when we are about to make a request
+  // second function is called automatically if there is an error with the request
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config; //returns the modified request object
+  },
+  (err) => {
+    return Promise.reject(err); //returns a rejected promise so that the error is passed to the next .catch block
+  }
+);
+
+export default instance;
